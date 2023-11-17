426084322f2f1a0aba79d0587a9f1204e3116a5d
Jiaqi Liu <jiaqi.3031@gmail.com>
2023-11-17 19:24:13 +0800

    plot more and refactor

--- src/make_features.py
+++ src/make_features.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
+
 steps_per_min = 12
+
+features = ['hour',
+        'anglez',
+        'enmo',
+        'anglez_rolling_mean_right_aligned_1',
+        'anglez_rolling_mean_left_aligned_1',
+        'enmo_rolling_mean_right_aligned_1',
+        'enmo_rolling_mean_left_aligned_1',
+        'anglez_rolling_mean_diff_1',
+        'enmo_rolling_mean_diff_1',
+        'anglez_rolling_mean_right_aligned_2',
+        'anglez_rolling_mean_left_aligned_2',
+        'enmo_rolling_mean_right_aligned_2',
+        'enmo_rolling_mean_left_aligned_2',
+        'anglez_rolling_mean_diff_2',
+        'enmo_rolling_mean_diff_2',
+        'anglez_rolling_std_centered_1',
+        'enmo_rolling_std_centered_1',
+        'anglez_rolling_std_centered_2',
+        'enmo_rolling_std_centered_2',
+        ]
 
 
 def make_features(df):
     return __make_features2(df)
 
 
-def __make_features1(df):
-    # It's important to drop timezone here, as there are different timezones in
-    # the train dataset.
-    df['timestamp'] = pd.to_datetime(
-        df['timestamp']).apply(lambda t: t.tz_localize(None))
-    df['hour'] = df['timestamp'].dt.hour
-    features = ['hour', 'anglez', 'enmo']
-    return df, features
-
-
 def __make_features2(df):
-    # It's important to drop timezone here, as there are different timezones in
-    # the train dataset.
-    df['timestamp'] = pd.to_datetime(
-        df['timestamp']).apply(lambda t: t.tz_localize(None))
-    df['hour'] = df['timestamp'].dt.hour
-
+    # Note that this window is between rows, not steps.
+    # So the interval here depends on the granularity of input dataframe.
     window = 1*steps_per_min
     for series_id in df['series_id'].unique():
         df.loc[df['series_id']==series_id, 'anglez_rolling_mean_right_aligned_1'] = df.loc[df['series_id']==series_id]['anglez'].rolling(window=window, min_periods=1).mean()
@@ -42,40 +50,12 @@ def __make_features2(df):
     df['anglez_rolling_mean_diff_2'] = df['anglez_rolling_mean_left_aligned_2'] - df['anglez_rolling_mean_right_aligned_2']
     df['enmo_rolling_mean_diff_2'] = df['enmo_rolling_mean_left_aligned_2'] - df['enmo_rolling_mean_right_aligned_2']
 
-    window = 30*steps_per_min
-    for series_id in df['series_id'].unique():
-        df.loc[df['series_id']==series_id, 'anglez_rolling_mean_right_aligned_3'] = df.loc[df['series_id']==series_id]['anglez'].rolling(window=window, min_periods=1).mean()
-        df.loc[df['series_id']==series_id, 'anglez_rolling_mean_left_aligned_3'] = df.loc[df['series_id']==series_id]['anglez'].rolling(window=window, min_periods=1).mean().shift(-window+1).fillna(method='ffill')
-        df.loc[df['series_id']==series_id, 'enmo_rolling_mean_right_aligned_3'] = df.loc[df['series_id']==series_id]['enmo'].rolling(window=window, min_periods=1).mean()
-        df.loc[df['series_id']==series_id, 'enmo_rolling_mean_left_aligned_3'] = df.loc[df['series_id']==series_id]['enmo'].rolling(window=window, min_periods=1).mean().shift(-window+1).fillna(method='ffill')
-    df['anglez_rolling_mean_diff_3'] = df['anglez_rolling_mean_left_aligned_3'] - df['anglez_rolling_mean_right_aligned_3']
-    df['enmo_rolling_mean_diff_3'] = df['enmo_rolling_mean_left_aligned_3'] - df['enmo_rolling_mean_right_aligned_3']
+    window = 1*steps_per_min
+    df['anglez_rolling_std_centered_1'] = df.groupby('series_id', as_index=False)['anglez'].rolling(window=window, min_periods=1, center=True).std()['anglez']
+    df['enmo_rolling_std_centered_1'] = df.groupby('series_id', as_index=False)['enmo'].rolling(window=window, min_periods=1, center=True).std()['enmo']
 
-    df['anglez_rolling_std_centered'] = df.groupby('series_id', as_index=False)['anglez'].rolling(window=1*steps_per_min, min_periods=1, center=True).std()['anglez']
-    df['enmo_rolling_std_centered'] = df.groupby('series_id', as_index=False)['enmo'].rolling(window=1*steps_per_min, min_periods=1, center=True).std()['enmo']
+    window = 10*steps_per_min
+    df['anglez_rolling_std_centered_2'] = df.groupby('series_id', as_index=False)['anglez'].rolling(window=window, min_periods=1, center=True).std()['anglez']
+    df['enmo_rolling_std_centered_2'] = df.groupby('series_id', as_index=False)['enmo'].rolling(window=window, min_periods=1, center=True).std()['enmo']
 
-    features = ['hour',
-            'anglez',
-            'enmo',
-            'anglez_rolling_mean_right_aligned_1',
-            'anglez_rolling_mean_left_aligned_1',
-            'enmo_rolling_mean_right_aligned_1',
-            'enmo_rolling_mean_left_aligned_1',
-            'anglez_rolling_mean_diff_1',
-            'enmo_rolling_mean_diff_1',
-            'anglez_rolling_mean_right_aligned_2',
-            'anglez_rolling_mean_left_aligned_2',
-            'enmo_rolling_mean_right_aligned_2',
-            'enmo_rolling_mean_left_aligned_2',
-            'anglez_rolling_mean_diff_2',
-            'enmo_rolling_mean_diff_2',
-            'anglez_rolling_mean_right_aligned_3',
-            'anglez_rolling_mean_left_aligned_3',
-            'enmo_rolling_mean_right_aligned_3',
-            'enmo_rolling_mean_left_aligned_3',
-            'anglez_rolling_mean_diff_3',
-            'enmo_rolling_mean_diff_3',
-            'anglez_rolling_std_centered',
-            'enmo_rolling_std_centered',
-            ]
-    return df, features
+    return df
--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -30,6 +30,20 @@ def load_dataset():
     return train
 
 
+def filter_dataset(df):
+    # It's important to drop timezone here, as there are different timezones in
+    # the train dataset.
+    df['timestamp'] = pd.to_datetime(
+        df['timestamp']).apply(lambda t: t.tz_localize(None))
+    df['hour'] = df['timestamp'].dt.hour
+    df['second'] = df['timestamp'].dt.second
+    # Using less data produces poor performance score.
+    # Let's comment it for now. That is, we still use full dataset.
+    # df = df[df['second'] == 0]
+    # df = df.reset_index(drop=True)
+    return df
+
+
 def split_into_train_and_validation(train):
     series_ids = train['series_id'].unique()
     # Don't do random split. The last 8 series have no events.
@@ -43,13 +57,13 @@ def split_into_train_and_validation(train):
 def extend_features(df):
     print('-'*50)
     print(f'df.shape: {df.shape}')
-    df, features = make_features.make_features(df)
-    X = df[features]
+    df = make_features.make_features(df)
+    X = df[make_features.features]
     y = df['awake']
     print(f'X.shape: {X.shape}')
     print(f'X.isnull().values.any(): {X.isnull().values.any()}')
     print('-'*50)
-    return X, y, features
+    return X, y
 
 
 def fit_classifier(X_train, y_train):
@@ -66,9 +80,9 @@ def fit_classifier(X_train, y_train):
     return rf_classifier
 
 
-def save_importance_plot(rf_classifier, features):
+def save_importance_plot(rf_classifier):
     fig, ax = plt.subplots(figsize=(8, 4))
-    ax.bar(features, rf_classifier.feature_importances_)
+    ax.bar(make_features.features, rf_classifier.feature_importances_)
     ax.tick_params(axis='x', rotation=-35)
     ax.set_title('Random forest feature importance')
     plt.xticks(rotation=-30, ha='left')
@@ -89,8 +103,8 @@ def save_prediction(rf_classifier):
     # It's OK. Actually we should never run for test file. It's useless.
     test = pd.read_parquet(
         '../child-mind-institute-detect-sleep-states/test_series.parquet')
-    test, features = make_features.make_features(test)
-    X_test = test[features]
+    test = make_features.make_features(test)
+    X_test = test[make_features.features]
     test['not_awake'] = rf_classifier.predict_proba(X_test)[:, 0]
     test['awake'] = rf_classifier.predict_proba(X_test)[:, 1]
     test['insleep'] = (test['not_awake'] > test['awake']).astype('bool')
@@ -98,15 +112,15 @@ def save_prediction(rf_classifier):
 
 
 def main():
-    train = load_dataset()
+    train = filter_dataset(load_dataset())
     train, val = split_into_train_and_validation(train)
     print('Begin to make features')
-    X_train, y_train, features = extend_features(train)
+    X_train, y_train = extend_features(train)
     print('Begin to fit')
     rf_classifier = fit_classifier(X_train, y_train)
-    save_importance_plot(rf_classifier, features)
+    save_importance_plot(rf_classifier)
     print('Begin to validate and predict')
-    X_val, _, _ = extend_features(val)
+    X_val, _ = extend_features(val)
     save_validation(rf_classifier, X_val, val)
     # save_prediction(rf_classifier)
 
