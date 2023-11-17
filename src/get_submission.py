8cc059b9bfc1ecb24408a41908957888b6549a28
Jiaqi Liu <jiaqi.3031@gmail.com>
2023-11-17 09:39:55 +0800

    Val score: 0.389

--- src/get_submission.py
+++ src/get_submission.py
@@ -5,6 +5,7 @@ import itertools
 
 @dataclass
 class Event:
+    index: int
     series_id: str
     step: int
     event: str
@@ -19,20 +20,25 @@ def get_submission(test, least_sleep_time, least_awake_time):
     all_events = pd.DataFrame(
         columns=['series_id', 'step', 'event', 'score'])
     for series_id in series_ids:
-        one_test = test[test['series_id'] == series_id].copy()
+        one_test = test[test['series_id'] == series_id]
         one_windows = []
         last_onset = None
         last_wakeup = None
-        for _, row in one_test.iterrows():
+        for idx, row in one_test.iterrows():
             if last_onset is None:
                 if row['insleep'] == True:
                     last_onset = Event(
-                        series_id, row['step'], 'onset', row['not_awake'])
+                        idx, series_id, row['step'], 'onset', row['not_awake'])
             else:
                 if row['insleep'] == False:
                     last_wakeup = Event(
-                        series_id, row['step'], 'wakeup', row['awake'])
+                        idx, series_id, row['step'], 'wakeup', row['awake'])
                     if last_wakeup.step - last_onset.step > least_sleep_time:
+                        # In pandas.loc[a:b], b is inclusive.
+                        score = one_test.loc[last_onset.index:last_wakeup.index-1][
+                            'not_awake'].median()
+                        last_onset.score = score
+                        last_wakeup.score = score
                         one_windows.append((last_onset, last_wakeup))
                     last_onset = None
                     last_wakeup = None
